d7928820ae1336d29ff807f995a61950b927dc8c
Bruce Steedman <bruce.steedman@gmail.com>
2014-08-23 20:56:34 +0100

    Add pinInfoxboxes to show descriptions for each location on click

--- web.rb
+++ web.rb
@@ -5,5 +5,6 @@ Sinatra::register Gon::Sinatra
 
 get '/map' do
   gon.plot_locations = [[49.178609, -2.224561],[49.179508, -2.225726]]
+  gon.plot_descriptions = ['description1', 'description2']
   erb :layout
 end
