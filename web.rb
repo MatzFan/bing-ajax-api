f016abffe96b00e5bedb2632893cff0967d62415
Bruce Steedman <bruce.steedman@gmail.com>
2014-08-23 16:54:30 +0100

    Pass 2D array of location coords - can not plot multiple pins (all with same label)

--- web.rb
+++ web.rb
@@ -4,6 +4,6 @@ require 'gon-sinatra'
 Sinatra::register Gon::Sinatra
 
 get '/map' do
-  gon.plot_location = [49.178609, -2.224561]
+  gon.plot_locations = [[49.178609, -2.224561],[49.179508, -2.225726]]
   erb :layout
 end
